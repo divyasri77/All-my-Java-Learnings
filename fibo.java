da78588224cfd94c2e27cbad0e2eb01afd299669
divyasri77 <68841372+divyasri77@users.noreply.github.com>
2020-07-29 11:34:44 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author divya
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;int c;
+        System.out.println(a);
+        System.out.println(b);
+         for(int i=0;i<10;i++)
+         {
+           c=a+b;
+           a=b;
+           b=c;
+           System.out.println(c);
+// TODO coSystem.out.printlnde application logic here
+            }
+    
+}
